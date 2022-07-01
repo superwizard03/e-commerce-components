6849bc6dfd7fe3189decc67656df05e19521183f
superwizard03 <superwizard96013@outlook.com>
2022-07-01 15:59:18 +0800

    create a new repo

---
+++ app.js
@@ -0,0 +1,6 @@
+import "./css/tailwind.css";
+import Alpine from 'alpinejs'
+
+window.Alpine = Alpine;
+
+Alpine.start();
---
+++ tailwind.config.js
@@ -0,0 +1,57 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '*.{html,js}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'beige': '#FCF6F3',
+        'white': '#FFFFFF',
+        'black': '#000000',
+        'fd-blue': '#2C90C2',
+        'fd-dark-blue': '#38445E',
+        'fd-light-blue': '#E8F4FA',
+        'fd-orange': '#FF7342',
+        'form-gray': '#EDEDED',
+        'sustainable': '#91B520',
+        'transparent': 'transparent',
+        'gray': '#898989',
+        'red': '#b00000',
+        'wild-sand': '#F5F5F5',
+        'provincial-pink': '#FDF6F3',
+        'wild-sand-dark': '#F7F7F7',
+      },
+      fontSize: {
+        nav: ['16px', {
+          letterSpacing: '0.01em',
+          lineHeight: '38px',
+        }],
+        s: ['12px', '15px'],
+        sbase: ['14px', {
+          letterSpacing: '0.02em',
+          lineHeight: '24px',
+        }],
+        base: ['20px', {
+          letterSpacing: '0.02em',
+          lineHeight: '38px',
+        }],
+        xbase: ['24px', {
+          letterSpacing: '0.01em',
+          lineHeight: '38px',
+        }],
+        normal: ['18px', {
+          letterSpacing: '0.02em',
+          lineHeight: '31px'
+        }],
+        l: ['25px', '32px'],
+        sl: ['28px', '42px'],
+        xl: ['35px', '54px'],
+        xxl: ['44px', '62px'],
+        xxxl: ['48px', '68px'],
+        superxl: ['54px', '72px'],
+      },
+    },
+  },
+  plugins: [],
+}
